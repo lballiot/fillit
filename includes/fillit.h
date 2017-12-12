ff3645477d33823f40f9907a60a972d2d03e714a
Lydie BALLIOT <lballiot@e2r6p18.42.fr>
2017-12-12 15:53:52 +0100

    Add with ft check in progress

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: lballiot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 11:02:27 by lballiot          #+#    #+#             */
-/*   Updated: 2017/12/12 13:54:06 by lballiot         ###   ########.fr       */
+/*   Updated: 2017/12/12 15:10:43 by lballiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@
 # include <fcntl.h>
 # include <stdio.h>
 
-typedef	struct	    struct_list
+typedef	struct		struct_list
 {
     char            *tetra;
     struct s_list   *next;
-}                   l_tetra;
+}					l_tetra;
 
 //int		ft_check_number_arg(int ac);
 
--- src/main.c
+++ src/main.c
@@ -17,39 +17,51 @@ int				ft_check(char *str)
 {
 	int i;
 	size_t sharp;
-	//	size_t backn = 0;
-	int j = 0;
+	size_t point = 0;
 
-	i = 0;
+	i = -1;
 	sharp = 0;
 	if (!str)
-		return (-1);
-	if (ft_strlen(str) > 545)
-		return (-1);
-	while (str)
 	{
-		if (str[i] != '.' && str[i] != '#' && str[i] != '\n')
-			return (-1);
-/*		while (str[i] != '\n')
-		{
-			j++;
-			i++;
-			printf("i : %d\n", i);
-			printf("j : %d\n", j);
-		}
-*/
-
-		printf("j : %d\n", j);
-		if (str[i] == '\n' && j != 4)
-			return (-1);
-		if (str[i] == '\n' && str[i + 1] == '\n')
-		{
-			sharp += 1;
-			i++;
-		}
-		i++;
+		write(1, "\nif nothing\n", 12);
+		return (-1);
 	}
-	write(1, "toto\n", 5);
+	if (ft_strlen(str) > 545)
+	{
+		printf("\nstrlen\n");
+		return (-1);
+	}
+	while (str[++i])
+	{
+		if (str[i] != '.' && str[i] != '#' && str[i] != '\n' && str[i] != '\0')
+		{
+			printf("\n!= . # /n\n");
+			return (-1);
+		}
+		if (str[i] == '#')
+		{
+			sharp++;
+		//	printf("# : %zu\n", sharp);
+		}
+		if (str[i] == '.')
+			point++;
+		if (str[i] == '\n' && str[i - 1] == '\n')
+		{
+			if (sharp != 4)
+			{
+				printf("\nsharp != 4\n");
+				return (-1);
+			}
+			if (point != 12)
+			{
+				printf("\npoint != 12\n");
+				return (-1);
+			}
+			sharp = 0;
+			point = 0;
+		}
+	}
+//	write(1, "toto\n", 5);
 	return (0);
 }
 
@@ -94,6 +106,7 @@ int				main(int ac, char **av)
 		return (1);
 	}
 	check = ft_check(str_tetra);
+	printf("check : %d\n", check);
 	if (check == -1)
 	{
 		ft_putstr_fd("error\n", 2);
