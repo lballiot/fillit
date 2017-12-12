b6b22650c1d38fed9ea3efecd751dd1875fcd074
Lydie BALLIOT <lballiot@e2r6p18.42.fr>
2017-12-12 11:57:40 +0100

    Add progress with ft check

--- src/main.c
+++ src/main.c
@@ -13,6 +13,29 @@ static int		ft_check_number_arg(int ac)
 	return (1);
 }
 
+char			*ft_check(char *str)
+{
+	int i;
+	size_t sharp;
+
+	i = 0;
+	sharp = 0;
+	if (!str)
+		return (NULL);
+	while (str)
+	{
+		if (str[i] == '\n' && str[i + 1] == '\n')
+		{
+			sharp += 1;
+			i++;
+		}
+		i++;
+	}
+	return (str);
+}
+
+
+
 int				main(int ac, char **av)
 {
 	int		open_file;
@@ -54,46 +77,46 @@ int				main(int ac, char **av)
 }
 
 /*
-static t_list	*ft_fill_struct(char *str)
-{
-	l_tetra *list;
-	l_tetra *begin;
+   static t_list	*ft_fill_struct(char *str)
+   {
+   l_tetra *list;
+   l_tetra *begin;
 
-	if (!(list = (l_tetra *)malloc(sizeof(l_tetra))))
-		return(NULL);
-	if (str)
-	{
+   if (!(list = (l_tetra *)malloc(sizeof(l_tetra))))
+   return(NULL);
+   if (str)
+   {
  * TODO : replace this fuckin' dynamic memory allocation by
  * by simple hard allocation because the size that must be 
  * allocated is always the same. Fuckin' bitch!
-		if (!(list->tetra = (char*)malloc(sizeof(21))))//
-			return (NULL);// 
-		list->tetra = ft_memcpy(list->tetra, (const void*)str, 20);
-		str = ft_str_counter(str);
-		list->next = list;
-		begin = list;
-	}
-	while (str)//
-	{
-		if (!(list->tetra = (char*)malloc(sizeof(21))))//
-			return (NULL);//
-		list->tetra = ft_memcpy(list->tetra, (const void*)str, 20);
-		str = ft_str_counter(str);
-		list->next = list;
-	}
-	if (!(str))
-		list->tetra = NULL;
-	list->next = NULL;
-	return (l_tetra);
-}
+ if (!(list->tetra = (char*)malloc(sizeof(21))))//
+ return (NULL);// 
+ list->tetra = ft_memcpy(list->tetra, (const void*)str, 20);
+ str = ft_str_counter(str);
+ list->next = list;
+ begin = list;
+ }
+ while (str)//
+ {
+ if (!(list->tetra = (char*)malloc(sizeof(21))))//
+ return (NULL);//
+ list->tetra = ft_memcpy(list->tetra, (const void*)str, 20);
+ str = ft_str_counter(str);
+ list->next = list;
+ }
+ if (!(str))
+ list->tetra = NULL;
+ list->next = NULL;
+ return (l_tetra);
+ }
 
-static char		*ft_str_counter(char *str)
-{
-	char *ptr_str;
+ static char		*ft_str_counter(char *str)
+ {
+ char *ptr_str;
 
-	ptr_str = str;
-	while (*ptr_str != '\0' && *ptr_str != '\n' && (*ptr_str - 1) != '\n')
-		ptr_str++;
-	return (ptr_str);
-}
-*/
+ ptr_str = str;
+ while (*ptr_str != '\0' && *ptr_str != '\n' && (*ptr_str - 1) != '\n')
+ ptr_str++;
+ return (ptr_str);
+ }
+ */
